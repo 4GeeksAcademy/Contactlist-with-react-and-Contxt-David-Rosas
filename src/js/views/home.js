5493e09b0ffc0eb36721325e8c57364a467fdf38
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-01-21 03:56:30 +0000

    Procesing

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,43 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+
+const urlContactList = "https://playground.4geeks.com/contact/agendas/Santiago"
+
+
+
+export const Home = () => {
+
+	const [contacts, setContacts] = useState ([]) 
+
+	async function getData() {
+		const urlContactList = "https://playground.4geeks.com/contact/agendas/Santiago"
+		try {
+			const response = await fetch(urlContactList)
+			if (!response.ok) {
+				throw new Error (`Response status: ${response.status}`);
+			}
+			const json = await response.json();
+			console.log(json.contacts);
+		}	catch (error) {
+			console.log (error.message);
+		}
+	}
+
+	// ---> variable que improta la informacion 
+	
+	useEffect (() => {
+		getData();
+	}, [])
+
+	// ---> lo que aparece en la pagina 
+	return <div className="text-center mt-5">
+		{ contacts.map((item) => 
+			<div className="" key={contacts} style={{border : "solid"}}>
+				<p>Hola</p>
+			</div>
+		)}
 	</div>
-);
+};
